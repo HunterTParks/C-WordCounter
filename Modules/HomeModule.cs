db1fda731970692d40ba9a8f100c06630791a908
Hunter Parks <hunter.thomas.parks@gmail.com>
2017-06-02 08:24:17 -0700

    add HomeModules routing page

---
+++ Modules/HomeModule.cs
@@ -0,0 +1,14 @@
+using Nancy;
+
+namespace CProject.Objects
+{
+  public class HomeModule : NancyModule
+  {
+    public HomeModule()
+    {
+      Get["/"] = _ => {
+        return View["index.cshtml"];
+      };
+    }
+  }
+}
