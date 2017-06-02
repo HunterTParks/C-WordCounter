4e386bbca4a49262e9ed8dc06b86db877bde312a
Hunter Parks <hunter.thomas.parks@gmail.com>
2017-06-02 08:27:48 -0700

    add Objects file and added RepeatCounter object file with a constructor

---
+++ Objects/RepeatCounter.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using System;
+
+namespace CProject.Objects
+{
+  public class RepeatCounter
+  {
+    private string _sentence;
+    private string _word;
+
+    public RepeatCounter(string sentence, string word)
+    {
+      _sentence = sentence;
+      _word = word;
+    }
+  }
+}
