9104bb0165686b3fc3e61ea6ea31d5a33f2ff503
Hunter Parks <hunter.thomas.parks@gmail.com>
2017-06-02 09:47:22 -0700

    add another test to testing page

--- Tests/RepeatCounterTest.cs
+++ Tests/RepeatCounterTest.cs
@@ -5,7 +5,14 @@ namespace CProject.Objects
   public class RepeatCounterTests
   {
     [Fact]
-    public void RepeatCounter_singleWord_wood()
+    public void RepeatCounter_singleWord_one()
+    {
+      RepeatCounter newRepeatCounter = new RepeatCounter("wood", "wood");
+      Assert.Equal(1, newRepeatCounter.HowManyWords());
+    }
+
+    [Fact]
+    public void RepeatCounter_sentence_three()
     {
       RepeatCounter newRepeatCounter = new RepeatCounter("wood wood wood", "wood");
       Assert.Equal(3, newRepeatCounter.HowManyWords());
